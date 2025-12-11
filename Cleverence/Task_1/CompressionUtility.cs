5b97ca56717e9212f0af8ef8be5b35a3fc4230cc
Артём Сампалов <sampalov167@gmail.com>
2025-12-12 02:57:45 +0300

    Решение задания #1

---
+++ Cleverence/Task_1/CompressionUtility.cs
@@ -0,0 +1,96 @@
+﻿using System.Text;
+
+namespace Cleverence.Task_1
+{
+    public class CompressionUtility
+    {
+        public void CompressString(string input)
+        {
+            if (StringValidator.IsValidNativeOriginal(input))
+            {
+                Console.WriteLine($"Полученная строка:\t{input}\t|\tСжатая строка:\t\t{CompressionAlgorithm(input)}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка валидации исходной строки");
+            }
+        }
+
+        public void DecompressString(string input)
+        {
+            if (StringValidator.IsValidNativeCompressed(input))
+            {
+                Console.WriteLine($"Полученная строка:\t{input}\t|\tИсходная строка:\t{DecompressionAlgorithm(input)}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка валидации сжатой строки");
+            }
+        }
+
+        private string CompressionAlgorithm(ReadOnlySpan<char> input)
+        {
+            StringBuilder sb = new();
+            sb.Append(input[0]);
+            int count = 1;
+
+            foreach (char c in input.Slice(1))
+            {
+                if (c == sb[sb.Length - 1])
+                {
+                    ++count;
+                }
+                else
+                {
+                    if (count > 1) sb.Append(count);
+                    sb.Append(c);
+                    count = 1;
+                }
+            }
+
+            if (count > 1) sb.Append(count);
+            return sb.ToString();
+        }
+
+        private string DecompressionAlgorithm(ReadOnlySpan<char> input)
+        {
+            StringBuilder sb = new();
+            StringBuilder count = new();
+
+            foreach (char c in input)
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    ApplyCount();
+                    sb.Append(c);
+                }
+                else if (c >= '0' && c <= '9')
+                {
+                    count.Append(c);
+                }
+            }
+
+            void ApplyCount()
+            {
+                if (count.Length > 0)
+                {
+                    if (int.TryParse(count.ToString(), out int result))
+                    {
+                        if (sb.Length > 0)
+                        {
+                            for (int i = 1; i < result; ++i)
+                            {
+                                sb.Append(sb[sb.Length - 1]);
+                            }
+                        }
+
+                        count.Clear();
+                    }
+                }
+            }
+
+            ApplyCount();
+            return sb.ToString();
+        }
+    }
+}
---
+++ Cleverence/Task_1/StringValidator.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Cleverence.Task_1
+{
+    public static class StringValidator
+    {
+        private static Regex OriginalString { get; } = new Regex("^[a-z]+$", RegexOptions.Compiled);
+        private static Regex CompressedString { get; } = new Regex("^([a-z][0-9]+)+$", RegexOptions.Compiled);
+
+        public static bool IsValidRegexOriginal(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return false;
+            return OriginalString.IsMatch(input);
+        }
+
+        public static bool IsValidRegexCompressed(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return false;
+            return CompressedString.IsMatch(input);
+        }
+
+        public static bool IsValidNativeOriginal(ReadOnlySpan<char> input)
+        {
+            if (input.IsEmpty) return false;
+
+            foreach (char c in input)
+            {
+                if (c < 'a' || c > 'z') return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsValidNativeCompressed(ReadOnlySpan<char> input)
+        {
+            if (input.IsEmpty) return false;
+
+            bool flag_f = false;
+            bool flag_s = false;
+
+            for (int i = 0; i < input.Length; ++i)
+            {
+                char c = input[i];
+
+                if (c >= 'a' && c <= 'z')
+                {
+                    if (flag_f && !flag_s) return false;
+
+                    flag_f = true;
+                    flag_s = (i == input.Length - 1) ? true : false;
+                }
+                else if (c >= '0' && c <= '9')
+                {
+                    flag_s = true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return flag_s;
+        }
+    }
+}
