6bfef4fea9f9b547a8d93aaac66f0b92d593e51a
Артём Сампалов <sampalov167@gmail.com>
2025-12-12 00:38:04 +0300

    Подготовка проекта: оформление иерархии файлов

---
+++ Cleverence/Tests/Task_2_Test.cs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleverence.Tests
+{
+    internal class Task_2_Test
+    {
+    }
+}
